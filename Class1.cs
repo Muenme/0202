d88d2f00f6dff2ae759015bdfd6052e5b7f02050
«Мэри <muenme.world@gmail.com>
2026-05-11 20:48:16 +0300

    +Trace

--- Class1.cs
+++ Class1.cs
@@ -1,8 +1,19 @@
-﻿namespace ClassLibrary
+﻿using System.Diagnostics;
+
+namespace ClassLibrary
 {
     public class Class1
     {
-        public double Addition(double a, double b) => a + b;
+        public Class1()
+        {
+            Trace.Listeners.Add(new TextWriterTraceListener("Log.txt"));
+            Trace.AutoFlush = true;
+        }
+        public double Addition(double a, double b)
+        {
+            Trace.WriteLine($"{a} + {b} = {a+b}");
+            return a + b;
+        }
         public double Substruct(double a, double b) => a - b;
         public double Сomposition(double a, double b) => a * b;
         public double Power(double a, double b) => Math.Pow(a, b);
